9b1266f7dc2bfee23e0a697004fbdab1e9af2c70
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-01-30 17:38:36 +0200

    add basic routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 function App() {
-  return <div>dev-education</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Layout</div>} />
+      <Route index element={<div>HomePage</div>} />
+      <Route path="tasks" element={<div>TasksPage </div>} />
+    </Routes>
+  );
 }
+
 export default App;
