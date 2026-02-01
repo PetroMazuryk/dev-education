5b9b4c30135dff0ff85cf374761d847239ed6f58
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-02-01 15:49:00 +0200

    add a SharedLayout component to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Layout</div>} />
-      <Route index element={<div>HomePage</div>} />
-      <Route path="tasks" element={<div>TasksPage </div>} />
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<div>HomePage</div>} />
+        <Route path="tasks" element={<div>TasksPage </div>} />
+      </Route>
     </Routes>
   );
 }
