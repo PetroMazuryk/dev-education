00704df9cbae890f385b44b81a6a66598c0a17e0
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-02-13 14:24:59 +0200

    add Dev Education text next to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,12 @@ export const Header = () => {
     <header className={styles.header}>
       <div className={styles.container}>
         <NavLink to="/" className={styles.logo}>
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" className={styles.logoImg} />
+
+          <div className={styles.logoText}>
+            <span className={styles.logoTop}>Dev</span>
+            <span className={styles.logoBottom}>Education</span>
+          </div>
         </NavLink>
         <nav className={styles.nav}>
           <NavLink to="/" className={getLinkClass}>
