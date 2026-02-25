7c0ab367ab395263f69146942aa93c5bb635b891
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-02-25 17:43:18 +0200

    add uninstall normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'modern-normalize/modern-normalize.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
