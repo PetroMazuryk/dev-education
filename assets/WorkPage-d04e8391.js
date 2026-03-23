d88f382f5d6d4f25fdcb97516620d6917b60ccc9
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-03-23 15:52:58 +0000

    Deploying to gh-pages from @ PetroMazuryk/dev-education@4cea595ff33ef1c8b68c897b895c844c381aa295 🚀

--- assets/WorkPage-d04e8391.js
+++ assets/WorkPage-d04e8391.js
@@ -1,11 +1,11 @@
-import{j as e,r as a}from"./index-86d571bf.js";import{L as c}from"./LessonBlock-3b046a44.js";const d="_wrapper_1jwtr_1",u="_label_1jwtr_18",m="_select_1jwtr_26",o={wrapper:d,label:u,select:m},p=({value:t,onChange:s,options:n})=>e.jsxs("div",{className:o.wrapper,children:[e.jsx("label",{className:o.label,children:"Оберіть підрозділ:"}),e.jsxs("select",{className:o.select,value:t,onChange:s,children:[e.jsx("option",{value:"",children:"Виберіть підрозділ:"}),n.map(i=>e.jsx("option",{value:i,children:i},i))]})]}),D=[{id:1,link:"",title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
+import{j as e,r as a}from"./index-c7a04896.js";import{L as c}from"./LessonBlock-80b80eed.js";const d="_wrapper_1jwtr_1",u="_label_1jwtr_18",m="_select_1jwtr_26",o={wrapper:d,label:u,select:m},p=({value:t,onChange:s,options:n})=>e.jsxs("div",{className:o.wrapper,children:[e.jsx("label",{className:o.label,children:"Оберіть підрозділ:"}),e.jsxs("select",{className:o.select,value:t,onChange:s,children:[e.jsx("option",{value:"",children:"Виберіть підрозділ:"}),n.map(i=>e.jsx("option",{value:i,children:i},i))]})]}),D=[{id:1,link:"",title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
 5 мл – 70 000 / 95 000 Lux
 10 мл -  100 000 / 130 000 Lux
 без чашок > 200 000 Lux
 1 кам – 4000 Lux
 4 кам - 6000 Lux
 `,solution:"",description:""},{id:2,link:"",title:" SA-24 Штативи (ROTA) ",requirements:[" "],starterCode:`1 мл , 2мл – 1200 шт
-5 мл - 440 шт
+5 мл - 560 шт
 10 мл - 360 шт `,solution:"",description:""},{id:3,link:"",title:"SA-24. Test set 5 разів - на початку зміни та через 20 годин. ",requirements:[" "],starterCode:`Без пригарів  5 добрі / 9 погані,
 З пригарами 5 добрі / 11 погані,
 перевірка на колірність плюс 1 ам №15,
