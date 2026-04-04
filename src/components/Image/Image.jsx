8ae996e0023a423a1bbad737c789bcb2f5b0bb55
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-04-04 16:06:06 +0300

    fix component image

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,13 +1,5 @@
 import styles from './Image.module.css';
 
-export const Image = ({ src, alt = 'image', width, height }) => {
-  return (
-    <img
-      className={styles.image}
-      src={src}
-      alt={alt}
-      width={width}
-      height={height}
-    />
-  );
+export const Image = ({ src, alt }) => {
+  return <img className={styles.image} src={src} alt={alt} />;
 };
