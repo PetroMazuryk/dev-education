f3b7b60b4c947adbe3e9801d55ccb43d95fc98f0
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-06-12 17:07:39 +0000

    Deploying to gh-pages from @ PetroMazuryk/dev-education@f7916f94284d418d13d627175a9534d23f4c8c75 🚀

--- assets/DepartmentSelect-41ccc3f7.js
+++ assets/DepartmentSelect-41ccc3f7.js
@@ -1,4 +1,4 @@
-import{c as K,g as re,r as S,j as p}from"./index-56027e5f.js";var V={exports:{}};(function(k){var E=typeof window<"u"?window:typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope?self:{};/**
+import{c as K,g as re,r as S,j as p}from"./index-706156f7.js";var V={exports:{}};(function(k){var E=typeof window<"u"?window:typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope?self:{};/**
  * Prism: Lightweight, robust, elegant syntax highlighting
  *
  * @license MIT <https://opensource.org/licenses/MIT>
