c3e61f1db13b6634d1fd4a2c0476e2801ee031e2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-06-23 00:05:44 +0300

    add a color set

--- src/data/village/village.js
+++ src/data/village/village.js
@@ -2,7 +2,7 @@ import { img } from '/src/utils/img';
 export const village = [
   {
     id: 1,
-    title: '',
+    title: 'Барвники село',
     requirements: [''],
     starterCode: ``,
     solution: ``,
@@ -10,6 +10,7 @@ export const village = [
     images: [
       img('images/village/coloring_1.jpg'),
       img('images/village/coloring_2.jpg'),
+      img('images/village/coloring_3.jpg'),
     ],
   },
 
