78435d9f98d82e7167fa20b0ef086911fc86bcbe
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-06-28 17:12:42 +0300

    add file bicycle

---
+++ src/data/bicycle/bicycle.js
@@ -0,0 +1,21 @@
+import { img } from '/src/utils/img';
+export const bicycle = [
+  {
+    id: 1,
+    title: 'Барвники село',
+    requirements: [''],
+    starterCode: ``,
+    solution: ``,
+    description: ``,
+    images: [img('images/')],
+  },
+
+  {
+    id: 99,
+    title: '',
+    requirements: [' '],
+    starterCode: ``,
+    solution: ``,
+    description: ``,
+  },
+];
