04851a729f9a21607d05f160014e0437c4b91f4d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-06-30 13:26:58 +0300

    add car degreaser

--- src/data/bicycle/bicycle.js
+++ src/data/bicycle/bicycle.js
@@ -3,7 +3,12 @@ export const bicycle = [
   {
     id: 1,
     title: 'Велосипед Sky Land ',
-    requirements: [''],
+    requirements: [
+      'Позначення For disc-F:160 R:140 означає:',
+      'F:160 = Front (переднє колесо) → диск 160 мм',
+      'R:140 = Rear (заднє колесо) → диск 140 мм',
+      '-------------------------------------',
+    ],
     starterCode: ``,
     solution: ``,
     description: ``,
