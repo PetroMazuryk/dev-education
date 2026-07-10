60ee0dbe973c3d15249dd36f159b540d9b2393e8
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-07-10 11:23:04 +0000

    Deploying to gh-pages from @ PetroMazuryk/dev-education@d1ec576880ef5a39d388d719ba4c30ecd4144e2a 🚀

--- assets/DepartmentSelect-9c5809aa.js
+++ assets/DepartmentSelect-9c5809aa.js
@@ -1,4 +1,4 @@
-import{c as K,g as re,r as E,j as d}from"./index-46147cce.js";var V={exports:{}};(function(A){var P=typeof window<"u"?window:typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope?self:{};/**
+import{c as K,g as re,r as E,j as d}from"./index-87c8702b.js";var V={exports:{}};(function(A){var P=typeof window<"u"?window:typeof WorkerGlobalScope<"u"&&self instanceof WorkerGlobalScope?self:{};/**
  * Prism: Lightweight, robust, elegant syntax highlighting
  *
  * @license MIT <https://opensource.org/licenses/MIT>
--- assets/WorkPage-7b2b6b7e.js
+++ assets/WorkPage-7b2b6b7e.js
@@ -1,4 +1,4 @@
-import{i as e,r as l,j as t}from"./index-46147cce.js";import{D as m,L as d}from"./DepartmentSelect-0e0e34ee.js";const u=[{id:1,title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
+import{i as e,r as l,j as t}from"./index-87c8702b.js";import{D as m,L as d}from"./DepartmentSelect-9c5809aa.js";const u=[{id:1,title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
 5 мл – 70 000 / 95 000 Lux
 10 мл -  100 000 / 130 000 Lux
 без чашок > 200 000 Lux
