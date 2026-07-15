51f202408d466127c67ff6bbf05e8e511ea05888
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-07-15 12:28:50 +0300

    add chain name

--- src/data/bicycle/bicycle.js
+++ src/data/bicycle/bicycle.js
@@ -2,17 +2,23 @@ import { img } from '/src/utils/img';
 export const bicycle = [
   {
     id: 1,
-    title: 'Велосипед Sky Land ',
+    title: 'Велосипед Sky Land',
     requirements: [
       'Позначення For disc-F:160 R:140 означає:',
       'F:160 = Front (переднє колесо) → диск 160 мм',
       'R:140 = Rear (заднє колесо) → диск 140 мм',
       '-------------------------------------',
     ],
+
+    images: [img('images/bicycle/brake_1.jpg')],
+  },
+  {
+    id: 2,
+    title: 'Велосипед Sky Land',
+    requirements: ['Ланцюг KMC Z7  ( 7 speed)'],
     starterCode: ``,
     solution: ``,
     description: ``,
-    images: [img('images/bicycle/brake_1.jpg')],
   },
 
   {
