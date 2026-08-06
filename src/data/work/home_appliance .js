b55c4f7360b5ab2fc9fbaaed61cf92ccf1a6f825
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-08-06 16:59:46 +0300

    add a photo of paint

--- src/data/work/home_appliance .js
+++ src/data/work/home_appliance .js
@@ -84,8 +84,7 @@ export const home_appliance = [
       'Шпаклівка фінішна акрилова у відрі Anserglob',
       '',
     ],
-
-    images: [img('images/home/')],
+    images: [img('images/home/farbex.png'), img('images/home/')],
   },
   {
     id: 99,
