be04f3c442c6f7e817ecaf04d2711c8f884bfc64
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2026-08-26 21:34:37 +0000

    Deploying to gh-pages from @ PetroMazuryk/dev-education@d72c9ef1cc3f4f1694713a251fa8ccdd14f3a335 🚀

--- assets/WorkPage-d8dbad7d.js
+++ assets/WorkPage-d8dbad7d.js
@@ -1,4 +1,4 @@
-import{i as e,r as l,j as t}from"./index-ab14b324.js";import{D as m,L as d}from"./DepartmentSelect-a31edc15.js";const u=[{id:1,title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
+import{i as e,r as l,j as t}from"./index-aec23959.js";import{D as m,L as d}from"./DepartmentSelect-b245a0ee.js";const u=[{id:1,title:"SA-24 Світловий потік з чашками 2 кам / 3 кам: ",requirements:[" "],starterCode:`1 мл, 2 мл -  50 000 / 70 000 Lux
 5 мл – 70 000 / 95 000 Lux
 10 мл -  100 000 / 130 000 Lux
 без чашок > 200 000 Lux
